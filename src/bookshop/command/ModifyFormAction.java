f966a284d0454852192f847344509d48518fe850
wjdhxheo <jeon7co1p@gmail.com>
2020-03-06 13:14:23 +0900

    전준영 / <사용자영역> 회원로그인.로그아웃 (회원인증) 기능 추가.

---
+++ src/bookshop/command/ModifyFormAction.java
@@ -0,0 +1,29 @@
+package bookshop.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bookshop.bean.LogonDataBean;
+import bookshop.bean.LogonDBBean;
+
+public class ModifyFormAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request,
+			HttpServletResponse response) throws Throwable {
+		// TODO Auto-generated method stub
+		request.setCharacterEncoding("utf-8");
+		
+		String id = request.getParameter("id");
+		String passwd = request.getParameter("passwd");
+
+		//회원 정보를 수정하기 위한 정보를 얻어냄
+		LogonDBBean manager = LogonDBBean.getInstance();
+		LogonDataBean m = manager.getMember(id,passwd); 
+		
+		request.setAttribute("m",m);
+		request.setAttribute("id", id);
+		request.setAttribute("type", new Integer(1));
+		return "/member/modifyForm.jsp";
+	}
+}
